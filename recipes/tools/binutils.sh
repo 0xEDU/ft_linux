65587235a0813a09f19088587cb6d202a85cdad8
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-07-20 21:32:42 -0300

    Add gcc and format binutils

--- recipes/tools/binutils.sh
+++ recipes/tools/binutils.sh
@@ -2,5 +2,26 @@ RECIPE_NAME="binutils-2.26"
 RECIPE_TAR="binutils-2.26.tar.bz2"
 
 build() {
-	echo "oi"
+	mkdir -v build
+	cd build
+	../configure --prefix=/tools \
+		--with-sysroot=$LFS \
+		--with-lib-path=/tools/lib \
+		--target=$LFS_TGT \
+		--disable-nls \
+		--disable-werror
+	make
+	mkdir -v /tools/lib && ln -sv lib /tools/lib64
+	make install
+
+}
+
+is_installed() {
+	local sample_file="$LFS_TGT-ld"
+
+	if [ -f "$LFS/tools/bin/$sample_file" ]; then
+		return 0
+	else
+		return 1
+	fi
 }
