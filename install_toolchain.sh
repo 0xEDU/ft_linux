613f80f45fad400d412488d5262970092ea4d785
Eduardo Tachotte <etachott@student.42sp.org.br>
2025-07-20 21:31:41 -0300

    Improve toolchain installation script

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -11,24 +11,36 @@ LC_ALL=POSIX
 LFS_TGT=$(uname -m)-ft-linux-gnu
 PATH=/tools/bin:/bin:/usr/bin
 RECIPE_DIR="$LFS/ft_linux/recipes/tools"
+MAKEFLAGS="-j 12"
+
+# Recipe installation loop
 
 for recipe in "$RECIPE_DIR"/*.sh; do
+	source "$recipe"
+
+	if is_installed; then
+		echo ">> Package $(basename "$recipe") already installed!"
+		continue
+	fi
+
 	echo
 	echo "============================================================"
 	echo "  Building: $(basename "$recipe")"
 	echo "============================================================"
-
-	source "$recipe"
+	echo
 
 	echo ">> Extracting package..."
 	tar -xf "$LFS_SOURCES/$RECIPE_TAR" -C $LFS_SOURCES
 
 	echo ">> Building package $RECIPE_NAME..."
+	cd "$LFS_SOURCES/$RECIPE_NAME"
 	build
 
 	echo ">> Cleaning up dir..."
+	cd "$LFS_SOURCES"
 	rm -rf "$LFS_SOURCES/$RECIPE_NAME"
 
+	echo 
 	echo "============================================================"
 	echo "  Finished: $(basename "$recipe")"
 	echo "============================================================"
